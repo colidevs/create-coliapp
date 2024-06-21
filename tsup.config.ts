1a59c653474e9eec2c7a1a31e4b740b018de4d30
pufi <thomas.r.pufahl@gmail.com>
2024-06-21 12:16:09 -0300

    refact

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,7 @@ import path from "node:path";
 import {fileURLToPath} from "node:url";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["index.ts"],
   splitting: false,
   sourcemap: true,
   clean: true,
