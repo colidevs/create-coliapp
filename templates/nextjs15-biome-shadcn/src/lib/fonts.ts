ede2b59506adf983a1bec78a5d05578c48233db4
pufi <thomas.r.pufahl@gmail.com>
2025-05-29 02:02:38 -0300

    fix fonts

--- templates/nextjs15-biome-shadcn/src/lib/fonts.ts
+++ templates/nextjs15-biome-shadcn/src/lib/fonts.ts
@@ -1,75 +1,14 @@
-import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
+import { Montserrat } from "next/font/google";
 
-export const Poppins = localFont({
-	variable: "--font-Poppins",
-	src: [
-		{
-			path: "../../public/fonts/poppins/Poppins-Regular.ttf",
-			weight: "400",
-			style: "normal",
-		},
-		{
-			path: "../../public/fonts/poppins/Poppins-Medium.ttf",
-			weight: "500",
-			style: "normal",
-		},
-		{
-			path: "../../public/fonts/poppins/Poppins-Bold.ttf",
-			weight: "700",
-			style: "normal",
-		},
-		// italic
-		{
-			path: "../../public/fonts/poppins/italic/Poppins-Italic.ttf",
-			weight: "400",
-			style: "italic",
-		},
-		{
-			path: "../../public/fonts/poppins/italic/Poppins-MediumItalic.ttf",
-			weight: "500",
-			style: "italic",
-		},
-		{
-			path: "../../public/fonts/poppins/italic/Poppins-BoldItalic.ttf",
-			weight: "700",
-			style: "italic",
-		},
-	],
+export const poppins = Poppins({
+	variable: "--font-poppins",
+	subsets: ["latin"],
+	weight: ["400", "500", "700"],
 });
 
-export const Montserrat = localFont({
-	variable: "--font-Montserrat",
-	src: [
-		{
-			path: "../../public/fonts/montserrat/Montserrat-Regular.ttf",
-			weight: "400",
-			style: "normal",
-		},
-		{
-			path: "../../public/fonts/montserrat/Montserrat-Medium.ttf",
-			weight: "500",
-			style: "normal",
-		},
-		{
-			path: "../../public/fonts/montserrat/Montserrat-Bold.ttf",
-			weight: "700",
-			style: "normal",
-		},
-		// italic
-		{
-			path: "../../public/fonts/montserrat/italic/Montserrat-Italic.ttf",
-			weight: "400",
-			style: "italic",
-		},
-		{
-			path: "../../public/fonts/montserrat/italic/Montserrat-MediumItalic.ttf",
-			weight: "500",
-			style: "italic",
-		},
-		{
-			path: "../../public/fonts/montserrat/italic/Montserrat-BoldItalic.ttf",
-			weight: "700",
-			style: "italic",
-		},
-	],
+export const montserrat = Montserrat({
+	variable: "--font-montserrat",
+	subsets: ["latin"],
+	weight: ["400", "500", "700"],
 });
